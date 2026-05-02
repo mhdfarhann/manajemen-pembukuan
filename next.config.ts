be570737194e38bb9fbc667a26ea3faad9e70747
mhdfarhann <meruaputeh10@gmail.com>
2026-05-02 22:19:42 +0700

    feat: add CORS headers configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    reactStrictMode: true,
+  reactStrictMode: true,
 
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin',  value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
